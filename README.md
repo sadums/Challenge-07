fb1242ad9c9ca8e0a675194296a0afa7e94ce36a
sadums <samjadams4@gmail.com>
2023-05-02 16:16:23 -0600

    concatenate responses into a single string

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Challenge-07
+# asdf
+
+## Table of Contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Credits](#Credits)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+
+## Description
+
+sdf
+
+## Installation
+
+sdf
+
+## Usage
+
+sdf
+
+## Credits
+
+sdf
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Contributing
+
+sdf
+
+## Tests
+
+sdf
+
--- index.js
+++ index.js
@@ -1,38 +1,87 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter a title for your README.',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'Enter a description for your README.',
-            name: 'description'
-        },
-        {
-            type: 'input',
-            message: 'Enter installation instructions for your README.',
-            name: 'installation'
-        },
-        {
-            type: 'input',
-            message: 'Enter usage information for your README.',
-            name: 'usage'
-        },
-        {
-            type: 'input',
-            message: 'Enter contribution guidelines for your README.',
-            name: 'contributing'
-        },
-        {
-            type: 'input',
-            message: 'Enter test instructions for your README.',
-            name: 'tests'
-        }
-    ])
-    .then((response) => {
-        
-    });
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter a title for your README.',
+        name: 'title'
+    },
+    {
+        type: 'list',
+        message: 'Would you like to include a table of contents?',
+        name: 'tableOfContents',
+        choices: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Enter a description for your README.',
+        name: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions for your README.',
+        name: 'Installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information for your README.',
+        name: 'Usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter credits for your README.',
+        name: 'Credits'
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your README.',
+        name: 'License',
+        choices: [
+            "The Unlicense",
+            "Mozilla Public License 2.0",
+            "MIT License",
+            "GNU Lesser General Public License v2.1",
+            "GNU General Public License v3.0",
+            "GNU General Public License v2.0",
+            "Eclipse Public License 2.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Boost Software License 1.0",
+            "BSD 3-Clause License",
+            "BSD 2-Clause License",
+            "Apache License 2.0",
+            "GNU Affero General Public License v3.0"
+          ]
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines for your README.',
+        name: 'Contributing'
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions for your README.',
+        name: 'Tests'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log(`Wrote data to ${fileName}`));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => writeToFile('README.md', generateMarkdown(response)));
+}
+
+// Function call to initialize app
+init();
